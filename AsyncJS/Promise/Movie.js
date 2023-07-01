03f56cffdaacac4d27c957fb61fb39e7a9d3a819
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-07-01 12:09:49 +0530

    july 1

---
+++ AsyncJS/Promise/Movie.js
@@ -0,0 +1,12 @@
+let goToMovie = (success, failure) => {
+    let acc_Bal = 1300;
+
+    acc_Bal > 500 ? success("Go to Movie")
+        : failure("Go To PG, Open VS Code")
+}
+//goToMovie(() => { }, () => { });
+goToMovie((msg) => {
+    console.log(msg)
+}, (err) => {
+    console.log(err)
+});
